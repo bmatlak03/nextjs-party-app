b05bff865cd0798508fb2fef5eec4710ef2d772b
Bartosz Matlak <bartosz.matlak@op.pl>
2024-01-23 18:51:18 +0100

    create some base structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,53 @@
+type UserExpense = {
+  name: string;
+  amount: number;
+};
+
+type UserExpenseMap = {
+  [uid: string]: UserExpense;
+};
+
 export default function Home() {
-  return <div>app</div>;
+  return (
+    <div>
+      <h1 className="text-center font-bold text-3xl text-gray-800 px-2 mt-2">
+        Calculate your expenses
+      </h1>
+
+      <div className="px-4">
+        <form>
+          <div className="md:flex md:justify-center md:items-center mt-5">
+            <div className="md:w-2/5">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Name
+              </label>
+              <input
+                name="name"
+                type="text"
+                className="w-full bg-gray-100 focus:bg-white focus:outline-none border border-gray-300 rounded py-2 px-4 block appearance-none leading-normal"
+                placeholder="Name"
+              />
+            </div>
+            <div className="md:w-2/5">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Amount
+              </label>
+              <input
+                name="amount"
+                type="number"
+                className="w-full bg-gray-100 focus:bg-white focus:outline-none border border-gray-300 rounded py-2 px-4 block appearance-none leading-normal"
+                placeholder="Amount"
+              />
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="bg-gray-800 hover:bg-gray-900 w-full mt-5 p-2 text-white uppercase font-bold rounded"
+          >
+            Add
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
