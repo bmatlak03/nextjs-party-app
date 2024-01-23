13fb94a492d7bf3c2d1f297a6b7888eb16b12c89
Bartosz Matlak <bartosz.matlak@op.pl>
2024-01-23 19:20:15 +0100

    add fetching data

---
+++ src/lib/data.ts
@@ -0,0 +1,27 @@
+import { unstable_noStore as noStore } from "next/cache";
+import { sql } from "@vercel/postgres";
+
+import { Expense } from "./definitions";
+
+export async function fetchExpense() {
+  noStore();
+  // Add noStore() here prevent the response from being cached.
+  // This is equivalent to in fetch(..., {cache: 'no-store'}).
+
+  try {
+    // Artificially delay a response for demo purposes.
+    // Don't do this in production :)
+
+    console.log("Fetching revenue data...");
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    const data = await sql<Expense>`SELECT * FROM expenses`;
+
+    console.log("Data fetch complete after 3 seconds.");
+
+    return data.rows;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch revenue data.");
+  }
+}
