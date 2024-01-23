4c7384bed81512f008bcb79749f8104503e5d6b1
Bartosz Matlak <bartosz.matlak@op.pl>
2024-01-23 19:48:06 +0100

    mount new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import ExpensesForm from "@/components/ExpensesForm/ExpensesForm";
+import ExpensesList from "@/components/ExpensesList/ExpensesList";
 import { fetchExpense } from "@/lib/data";
 
 type UserExpense = {
@@ -18,40 +20,8 @@ export default async function Home() {
         Calculate your expenses
       </h1>
 
-      <div className="px-4">
-        <form>
-          <div className="md:flex md:justify-center md:items-center mt-5">
-            <div className="md:w-2/5">
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-                Name
-              </label>
-              <input
-                name="name"
-                type="text"
-                className="w-full bg-gray-100 focus:bg-white focus:outline-none border border-gray-300 rounded py-2 px-4 block appearance-none leading-normal"
-                placeholder="Name"
-              />
-            </div>
-            <div className="md:w-2/5">
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-                Amount
-              </label>
-              <input
-                name="amount"
-                type="number"
-                className="w-full bg-gray-100 focus:bg-white focus:outline-none border border-gray-300 rounded py-2 px-4 block appearance-none leading-normal"
-                placeholder="Amount"
-              />
-            </div>
-          </div>
-          <button
-            type="submit"
-            className="bg-gray-800 hover:bg-gray-900 w-full mt-5 p-2 text-white uppercase font-bold rounded"
-          >
-            Add
-          </button>
-        </form>
-      </div>
+      <ExpensesForm />
+      <ExpensesList />
     </div>
   );
 }
