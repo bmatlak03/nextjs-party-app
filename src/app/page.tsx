fb27705fcb55498a5812ad08ad68558318f7c219
Bartosz Matlak <bartosz.matlak@op.pl>
2024-01-23 20:25:40 +0100

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,7 @@
 import ExpensesForm from "@/components/ExpensesForm/ExpensesForm";
 import ExpensesList from "@/components/ExpensesList/ExpensesList";
-import { fetchExpense } from "@/lib/data";
-
-type UserExpense = {
-  name: string;
-  amount: number;
-};
-
-type UserExpenseMap = {
-  [uid: string]: UserExpense;
-};
 
 export default async function Home() {
-  const expenses = await fetchExpense();
-  console.log("expenses", expenses);
   return (
     <div>
       <h1 className="text-center font-bold text-3xl text-gray-800 px-2 mt-2">
