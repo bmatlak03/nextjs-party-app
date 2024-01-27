3444b3683a6bfbe610aa5f5b6d515f6c166af9f1
Bartosz Matlak <bartosz.matlak@op.pl>
2024-01-27 12:58:35 +0100

    limit container width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
   const calculatedDebts = calculateUsersDebts(expenses);
 
   return (
-    <div>
+    <div className="max-w-md mx-auto">
       <h1 className="text-center font-bold text-3xl text-gray-800 px-2 mt-2">
         Calculate your expenses
       </h1>
